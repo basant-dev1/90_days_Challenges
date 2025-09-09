e494894fd63af77e535d56e4c8ad6689de2fc728
basant-dev1 <official.kbasant.me@gmail.com>
2025-09-09 17:16:08 +0530

    Days 2 Practices
    # Conflicts:
    #	README.md

--- lib/days/days_2.dart
+++ lib/days/days_2.dart
@@ -1,3 +1,46 @@
 void main(){
   print('Days 2 Practices');
+  /* Operator and its Types
+  Operator :- Operator is the symbol used to perform operation on the variable and perform the task
+
+  Types
+  1) Arithmetic Operator
+      Addition    (+)
+      Substraction  (-)
+      Multiplication  (*)
+      Division        (/)
+      Integer Division  (~/)  Integer Value
+      Modulus       (%)         Reminder
+   */
+
+  //Examples
+  int a = 10;
+  int b = 3;
+  // print("Arithmetic Operator Example ${a+b}");
+
+  /* Logical Operator
+      AND
+      OR
+      NOT
+   */
+
+  /*Relational Operator
+    ==    Equal to Operator
+    !=    not equal to Operator
+    >     greater then
+    <     less then
+    >=    greater then equal to
+    <=    less then equal to
+
+   */
+
+  /*Assignment Operator
+     =   assign the value
+    +=
+    -=
+    *=
+    /=
+    %=
+
+   */
 }
